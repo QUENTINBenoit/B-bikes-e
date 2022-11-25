611c02e7242b384b5634f17eb6629ce155758715
github_QUENTINBenoit <benquelm@gmail.com>
2022-11-25 13:49:16 +0100

    creation de la  routes detailsProduct

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -7,16 +7,35 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+
+#[Route('/produit', name: 'product_',)]
 class ProductController extends AbstractController
 {
-    #[Route('/produit', name: 'product', methods:['GET'])]
-    public function showAllProduct( ProduitsRepository  $produitsRepository): Response
-    {
-           $ben = $produitsRepository->findAll();
-           dump($ben); 
+    /**
+     * Methode permettant d'affiche tout les produits
+     *
+     * @param ProduitsRepository $produitsRepository
+     * @return Response
+     */
+    #[Route('/list', name: 'list', methods:['GET'])]
+        public function showAllProduct( ProduitsRepository  $produitsRepository): Response
+        {
+           $produits = $produitsRepository->findAll();
+           dump($produits); 
 
         return $this->render('product/listProduits.html.twig', [
-            'product' => $ben,
+            'product' => $produits,
         ]);
-    }
+        }
+
+       
+    #[Route('/details', name:'details', methods:['GET'])] 
+        public function detailsProduct(): Response
+        {
+        \dump('route de details produits '); 
+        return $this->render('product/detailsProduit.html.twig');    
+       
+        }
+
+
 }
